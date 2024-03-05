5c8c6c2f78062afb75a06d6a096b88c97128edaf
PeterSolution <141070789+PeterSolution@users.noreply.github.com>
2024-03-05 23:24:47 +0100

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//Szablon elementu Pusta strona jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x415
+
+namespace waluty
+{
+    /// <summary>
+    /// Pusta strona, która może być używana samodzielnie lub do której można nawigować wewnątrz ramki.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        const string link = "https://static.nbp.pl/dane/kursy/Archiwum/archiwum_tab_a_2024.xls";
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Task.Run(async () =>
+            {
+                HttpClient client = new HttpClient();
+                string data = await client.GetStringAsync(new Uri(link));
+            });
+        }
+    }
+}
---
+++ c1.cs
@@ -0,0 +1,6 @@
+public class pozycjatabelia
+{
+    public string przelicznik { get; set; }
+    public string kod { get; set; }
+    public string kurs { get; set; }
+}
